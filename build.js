f581ecc97788a8a9f283f06c2e6e5202670b0088
at15 <at15@outlook.com>
2014-09-17 16:39:41 +0800

    Build the new emotion css and png

--- build.js
+++ build.js
@@ -1,22 +1,43 @@
 /**
  * Created by at15 on 14-9-17.
  */
+
+
 // build the emojis
 
-var nsg = require('node-sprite-generator');
+var gm = require('gm').subClass({imageMagick: true});
+var glob = require('glob');
+var path = require('path');
+var fs = require('fs');
 
-nsg({
-    src: [
-        'emotions/*.png'
-    ],
-    spritePath: 'build/emoji.png',
-    stylesheet:'css',
-    stylesheetPath: 'build/emoji.css'
-}, function (err) {
-    if (!err) {
-        console.log('Sprite generated!');
+var files = glob.sync('emotion_compressed/*', {});
 
-    } else {
-        console.log('error occurred', err);
-    }
-});
+// 首先复制第一个图片
+var first_img_path = 'build/emoji.png';
+var first_name = path.basename(files[0], '.png');
+var first_img_content = fs.readFileSync(files[0]);
+fs.writeFileSync(first_img_path, first_img_content);
+
+// 只能用eval了...
+var to_eval = 'gm(first_img_path).append(';
+var array_str = 'var emoji_list = [\'' + first_name + '\',\n';
+var css_str = '.' + first_name + '{background-position:0 0;}\n';
+for (var i = 1; i < files.length; i++) {
+    //gm(first_img_path).append(files[i]);
+    to_eval += "'" + files[i] + "',";
+    // 然后就是生成css了.
+    var file_name = path.basename(files[i], '.png');
+    var pos = {x: i * 30, y: 0};
+    array_str += "'" + file_name + "',\n";
+    css_str += '.' + file_name + '{background-position: ' + pos.x + 'px ' + pos.y + 'px;}\n';
+}
+
+array_str += ']';
+
+to_eval += 'true).write(first_img_path, function () {});';
+
+//console.log(to_eval);
+eval(to_eval);
+
+fs.writeFileSync('build/emoji.css', css_str);
+fs.writeFileSync('build/emoji.arr.js', array_str);
