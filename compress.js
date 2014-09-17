4d30e3a0f14c9946436c03a8bcb48f7ab358008e
at15 <at15@outlook.com>
2014-09-17 15:21:34 +0800

    We can compress use gm now
    in fact we dont need the node sprite any more.....

--- compress.js
+++ compress.js
@@ -4,25 +4,33 @@
 
 // use image magic to compress the image
 
-var gm = require('gm');
+var gm = require('gm').subClass({imageMagick: true});
 var glob = require('glob');
-var path =require('path');
+var path = require('path');
+var fs = require('fs');
 
 var opt = {
-    src:'emotions/*.png',
-    dst:'emotion_compressed'
+    src: 'emotions/*.png',
+    dst: 'emotion_compressed'
 };
 
 var files = glob.sync(opt.src, {});
 
-files.forEach(function(file){
+files.forEach(function (file) {
     //console.log(file);
+    var file_name = path.basename(file, '.png');
+    //console.log(file_name);
+    //var writeStream = fs.createWriteStream(opt.dst + '/' + file_name + '.png', {
+    //    flags: 'w',
+    //    encoding: null,
+    //    mode: 0666
+    //});
+
     gm(file)
-        .resize(30 , 30)
-        .autoOrient()
-        .write(writeStream, function (err) {
+        .resize(30, 30)
+        .write(opt.dst + '/' + file_name + '.png', function (err) {
             if (err) {
-                console.log(' compress fail',err);
+                console.log(' compress fail', err);
             }
         });
 });
